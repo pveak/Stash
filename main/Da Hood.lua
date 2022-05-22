b9df98bc260fd4ac8bdd80de2f3a32959307b181
pveak <104019210+pveak@users.noreply.github.com>
2022-05-22 12:07:05 +0100

    Create Da Hood.lua

---
+++ main/Da Hood.lua
@@ -0,0 +1,27 @@
+-- Team check
+
+local AimingIgnored = Aiming.Ignored
+AimingIgnored.TeamCheck(false)
+
+local AimingSettings = Aiming.Settings
+AimingSettings.Ignored.IgnoreLocalTeam = false
+
+-- check if down
+local AimingChecks = Aiming.Checks
+local AimingUtilities = Aiming.Utilities
+function AimingChecks.Custom(Player)
+  
+local Character = AimingUtilities.Character(Player)
+local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
+local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
+  
+  
+ -- check 2
+   if (KOd or Grabbed) then
+        return false
+    end
+    return true
+end
+
+-- return
+return Aiming
